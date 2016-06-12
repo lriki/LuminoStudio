369270a6a3c47aaf6fb6c553a34865b40352f5ff
lriki <lriki.net@gmail.com>
2016-06-12 14:42:45 +0900

    initial

---
+++ Engine/LuminoStudio/LuminoStudio/Views/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LuminoStudio.Views
+{
+    /* 
+	 * ViewModelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedWeakEventListenerや
+     * CollectionChangedWeakEventListenerを使うと便利です。独自イベントの場合はLivetWeakEventListenerが使用できます。
+     * クローズ時などに、LivetCompositeDisposableに格納した各種イベントリスナをDisposeする事でイベントハンドラの開放が容易に行えます。
+     *
+     * WeakEventListenerなので明示的に開放せずともメモリリークは起こしませんが、できる限り明示的に開放するようにしましょう。
+     */
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HwndSource source = (HwndSource)HwndSource.FromVisual(this);
+
+            Lumino.Config.SetUserWindowHandle(source.Handle);
+            Lumino.Engine.Initialize();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Lumino.Engine.Terminate();
+        }
+    }
+}
