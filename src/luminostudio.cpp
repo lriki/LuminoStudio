837c597663f5bb17b84c3098e91c990668562508
lriki <lriki.net@gmail.com>
2017-01-01 21:10:29 +0900

    tab

--- src/luminostudio.cpp
+++ src/luminostudio.cpp
@@ -27,6 +27,18 @@ LuminoStudio::LuminoStudio(QWidget *parent)
 	fileMenu->addAction(saveAction);
 	fileMenu->addSeparator();
 	fileMenu->addAction(quitAction);
+
+	QTabWidget* tabWidget = new QTabWidget(this);
+	QTextEdit* editor1 = new QTextEdit(tr("1"), tabWidget);
+	QTextEdit* editor2 = new QTextEdit(tr("2"), tabWidget);
+	tabWidget->addTab(editor1, tr("TAB1"));
+	tabWidget->addTab(editor2, tr("TAB2"));
+	//tabWidget->setTabClosable(true);
+	tabWidget->setTabsClosable(true);
+	tabWidget->setMovable(true);
+	setCentralWidget(tabWidget);
+
+
 }
 
 LuminoStudio::~LuminoStudio()
